21c1db0330d99048734e5fe3ee75bc180acb4ddb
Captain <davidbanville4@gmail.com>
2019-04-15 00:05:24 -0400

    dynamic divs woot

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+   
+   
+    var fighters = [
+        {Name: 'Han Solo', Image:'assets/images/Hansolo.jpg', Health: '120', Attack:'8',AttackBonus:'8',Counter:'6'},
+        {Name: 'Greedo', Image:'assets/images/Greedo.jpg', Health: '80', Attack:'9',AttackBonus:'9',Counter:'5'},
+        {Name: 'Boba Fett', Image:'assets/images/Bobafett.jpeg', Health: '180', Attack:'6',AttackBonus:'6',Counter:'25'},
+        {Name: 'Lando Calrissian', Image:'assets/images/Lando.png', Health: '130', Attack:'7',AttackBonus:'7',Counter:'20'},
+        // {Name: 'Jar Jar Binks', Image:'assets/images/JarJar.jpg', Health: '200', Attack:'50',AttackBonus:'50',Counter:'40'},
+    ];
+    
+    var chosenOne = fighters[i];
+    // Dynamically making bootsrap card in jquery ftw!
+        for (var i = 0; i < fighters.length; i++){
+        var column = $("<div>");
+        $(column).addClass("col-md-3");
+        $("#row-one").append(column); 
+        var character = $("<button>");
+        $(character).addClass(fighters[i].Name);
+        $(character).addClass("card");
+        $(column).append(character);
+        var image = $("<img>");
+        $(image).addClass("card-img-top")
+        $(image).attr("src" , fighters[i].Image);
+        $(character).append(image);
+        var cardBody = $("<div>");
+        $(cardBody).addClass("card-body");
+        $(character).append(cardBody);
+        var cardText = $("<p>");
+        $(cardText).addClass("card-text");
+        $(character).append(cardText);
+        $(cardText).text(fighters[i].Name + " " + fighters[i].Health);
+        
+        
+        }
+
+        $(".card").on("click",function(){
+            if ($(this).hasClass(fighters[0].Name)){
+            console.log("I am Han Solo");
+            }
+            else if ($(this).hasClass(fighters[1].Name)){
+            console.log("I am Greedo");
+            }
+           else  if ($(this).hasClass(fighters[2].Name)){
+            console.log("I am Boba Fett");
+           }
+            else if ($(this).hasClass(fighters[3].Name)){
+            console.log("I am Lando");
+            }
+        });
+    
+    
+});
+
+
